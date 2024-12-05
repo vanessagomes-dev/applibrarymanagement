c53a5af9c55b3e034e4e5a4200d6a2af76d47b50
vanessa <vanessagomesdev@gmail.com>
2024-12-05 20:22:49 -0300

    "Corrigidos erros de importação e ajustes na estrutura de modulos"

---
+++ frontend/library-management-frontend/src/app/home/home.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [] 
+})
+export class HomeModule {}
